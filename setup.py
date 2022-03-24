7eeccca1a5d2a48f6fa24ec64c23b226d5517c82
Nadia Carvalho <nadiacarvalho118@gmail.com>
2022-03-24 14:22:54 +0000

    package for imapy

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='IMAPY',
+    version='0.1dev',
+    packages=['imapy',],
+
+    long_description=open('README.md').read(),
+
+    url='https://github.com/NadiaCarvalho/IMAPY',
+    author='Nádia Carvalho',
+    author_email='nadiacarvalho118@gmail.com',
+    license='GNU',
+)
---
+++ tests/test.py
@@ -0,0 +1,11 @@
+from imapy import IMA
+
+if __name__ == '__main__':
+    onsets = [0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 14, 16, 17, 18, 20, 21, 22, 24, 25, 26, 32, 33, 34, 36, 37, 38, 39, 40, 41, 42, 44, 46, 48, 49, 50, 51, 52, 53, 54, 56, 57, 58]
+
+    ima_calculator = IMA(onsets=onsets)
+
+    # Recalculate Weights with different parameters:
+    #ima_calculator.calculateWeights(onsets, 3, 2, 1, 2, 1)
+
+    print(ima_calculator.calculate_IMA_score())
